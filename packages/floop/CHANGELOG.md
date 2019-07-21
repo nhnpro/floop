fe733ee4dfa476fdf91d54e7062e6164be72b9b0
Ignacio <icata2016@gmail.com>
2019-07-20 21:00:04 -0400

    expose internals

--- packages/floop/CHANGELOG.md
+++ packages/floop/CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.2.1] - 20/07/2019
+
+Exposes the internals of the library.
+
 ## [0.2.0] - 09/07/2019
 
 Update Docs, readme, reworks file organization.
---
+++ packages/floop/lib/internals.dart
@@ -0,0 +1,9 @@
+/// Exposes the internal classes of Floop in case there is interest in
+/// modifying or extending the behavior of the library.
+///
+/// floop.dart reprents the public API and should be the regular import.
+
+export 'package:floop/src/observed.dart';
+export 'package:floop/src/mixins.dart';
+export 'package:floop/src/controller.dart';
+export 'package:floop/src/repeater.dart';
